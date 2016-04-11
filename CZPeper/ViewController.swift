18d2671c507e60df2c04e653b9d0fd5e794eab11
xiaocuihero <xiaocuihero1@163.com>
2016-04-11 14:22:14 +0800

    Initial Commit

---
+++ CZPeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CZPeper
+//
+//  Created by Cui on 16/4/11.
+//  Copyright © 2016年 Cui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
